4a6007c26de173a3e10e62c43194027939d799a6
iraj <vazhenegar@hotmail.com>
2021-02-20 12:23:19 +0330

    fix width of slideshow

--- resources/views/PageElements/Main/Slider.blade.php
+++ resources/views/PageElements/Main/Slider.blade.php
@@ -1,18 +1,4 @@
 {{--<!-- Start Slider Home -->--}}
-{{--<section class="bg_secondry">--}}
-{{--    <canvas id="particles-js"></canvas>--}}
-{{--    <div class="home-content">--}}
-{{--        <div class="text-center banner-center-content">--}}
-{{--            <h4>نحن نتميز</h4>--}}
-{{--            <h1>تصميم <span id="js-rotating">خلاق, جذاب, حديث</span> للقوالب</h1>--}}
-{{--            <p>نحن وكالة تصميم رقمية متحمسة متخصصة في تصميم رقمي جميل وسهل الاستخدام. خدمات تطوير الويب.</p>--}}
-{{--            <a href="#" class="button btn-default">ابدأ الآن</a>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <a class="scroll-down" title="Scroll Down" href="#" data-scroll-nav="1"><i class="fa fa-circle" aria-hidden="true"></i></a>--}}
-{{--</section>--}}
-{{--<!--// End Slider Home-->--}}
-
 
     <style>
         /* reset */
@@ -156,7 +142,7 @@
 
         .slideshow {
             position: absolute;
-            width: 100vw;
+            width: 100%;
             height: 100vh;
             overflow: hidden;
         }
@@ -403,5 +389,6 @@
     <div class="slideshow-image" style="background-image: url('https://unsplash.it/1600/1400?image=483')"></div>
     <div class="slideshow-image" style="background-image: url('https://unsplash.it/1600/1400?image=478')"></div>
 </div>
-</section>
 <a class="scroll-down" title="Scroll Down" href="#" data-scroll-nav="1"><i class="fa fa-circle" aria-hidden="true"></i></a>
+</section>
+{{--<!--// End Slider Home-->--}}
